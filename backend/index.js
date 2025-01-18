113acdd6ed3a73970dacf41c128c42997881e828
vishnu-prasath-M <imvishnuprasath@gmail.com>
2025-01-18 17:35:47 +0000

    Updated Mern App

--- backend/index.js
+++ backend/index.js
@@ -173,5 +173,5 @@ app.get('/category/:category', async (req, res) => {
   }
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
